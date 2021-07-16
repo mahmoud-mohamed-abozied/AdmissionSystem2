b2e8ef9bcb427a9950e6056238439f11007fab50
Mahmoud Mohamed Abozied <mmabozied22@stud.fci-cu.edu.eg>
2021-07-16 02:05:46 +0200

    Add Paging

---
+++ Models/ApplicationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdmissionSystem2.Models
+{
+    public class ApplicationDto
+    {
+        public int Id;
+        public DateTime AdmissionDate;
+        public String Status;
+        public String AppliantName;
+    }
+}
---
+++ Services/AdminRepo.cs
@@ -0,0 +1,26 @@
+﻿using AdmissionSystem2.Entites;
+using AdmissionSystem2.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdmissionSystem2.Services
+{
+    public class AdminRepo : IAdminRepo
+    {
+        private AdmissionSystemDbContext _AdmissionSystemDbContext;
+
+        public AdminRepo(AdmissionSystemDbContext admissionSystemDbContext)
+        {
+            _AdmissionSystemDbContext = admissionSystemDbContext;
+        }
+        public IEnumerable<Application> GetApplication(ResourceParameters resourceParameters)
+        {
+            return _AdmissionSystemDbContext.Appliaction
+                .Skip(resourceParameters.PageSize*(resourceParameters.PageNumber-1))
+                .Take(resourceParameters.PageSize)
+                .ToList();
+        }
+    }
+}
